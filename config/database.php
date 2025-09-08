b7ecfe3f621290da0e9e555eca649860614c1daa
Mohamed Mufeed <mufeedbinismail@gmail.com>
2025-09-08 22:51:33 +0400

    compatibility fix: Initialized laravel, database modes

--- config/database.php
+++ config/database.php
@@ -56,7 +56,11 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false,
+            'modes' => [
+                'STRICT_TRANS_TABLES',
+                // Add other modes you want here
+            ],
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
