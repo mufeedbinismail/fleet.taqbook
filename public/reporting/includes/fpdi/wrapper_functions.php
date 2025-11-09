1b68e3cdb2d97f46b86eabff4a015f846f6cb0b0
Mohamed Mufeed <mufeedbinismail@gmail.com>
2025-11-09 15:27:23 +0400

    Dropped support for PHP version below 7

--- public/reporting/includes/fpdi/wrapper_functions.php
+++ public/reporting/includes/fpdi/wrapper_functions.php
@@ -18,25 +18,13 @@
 //
 
 
-if (!defined("PHP_VER_LOWER43")) 
-	define("PHP_VER_LOWER43", version_compare(PHP_VERSION, "4.3", "<"));
-
-
 /**
  * ensure that strspn works correct if php-version < 4.3
  */
 function _strspn($str1, $str2, $start=null, $length=null) {
     $numargs = func_num_args();
 
-    if (PHP_VER_LOWER43 == 1) {
-        if (isset($length)) {
-            $str1 = substr($str1, $start, $length);
-        } else {
-            $str1 = substr($str1, $start);
-        }
-    }
-
-    if ($numargs == 2 || PHP_VER_LOWER43 == 1) {
+    if ($numargs == 2) {
         return strspn($str1, $str2);
     } else if ($numargs == 3) {
         return strspn($str1, $str2, $start);
@@ -52,15 +40,7 @@ function _strspn($str1, $str2, $start=null, $length=null) {
 function _strcspn($str1, $str2, $start=null, $length=null) {
     $numargs = func_num_args();
 
-    if (PHP_VER_LOWER43 == 1) {
-        if (isset($length)) {
-            $str1 = substr($str1, $start, $length);
-        } else {
-            $str1 = substr($str1, $start);
-        }
-    }
-
-    if ($numargs == 2 || PHP_VER_LOWER43 == 1) {
+    if ($numargs == 2) {
         return strcspn($str1, $str2);
     } else if ($numargs == 3) {
         return strcspn($str1, $str2, $start);
