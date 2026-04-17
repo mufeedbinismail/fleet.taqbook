cc37e127a502fc5e5df98e8afce8d0fce33ba1b9
Mohamed Mufeed <mufeedbinismail@gmail.com>
2026-04-17 09:06:37 +0530

    feat: store marketplace tax setting & refresh ui when changing marketplace

--- public/sales/sales_order_entry.php
+++ public/sales/sales_order_entry.php
@@ -340,7 +340,6 @@ function copy_to_cart()
 	$cart->customer_id	= $_POST['customer_id'];
 	$cart->Branch = $_POST['branch_id'];
 	$cart->sales_type = $_POST['sales_type'];
-    $cart->marketplace_id = get_post('marketplace_id');
 
 	if ($cart->trans_type!=ST_SALESORDER && $cart->trans_type!=ST_SALESQUOTE) { // 2008-11-12 Joe Hunt
 		$cart->dimension_id = $_POST['dimension_id'];
