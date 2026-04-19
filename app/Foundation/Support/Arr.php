98087bb3cd0f32726c5ec4f9e350ee534f42c477
Mohamed Mufeed <mufeedbinismail@gmail.com>
2026-04-19 07:41:35 +0530

    feat: added support for bracketSet, bracketGet from array

--- app/Foundation/Support/Arr.php
+++ app/Foundation/Support/Arr.php
@@ -2,7 +2,6 @@
 
 namespace App\Foundation\Support;
 
-
 class Arr extends \Illuminate\Support\Arr
 {
     const NOT_SET = '__NOT_SET__';
@@ -19,5 +18,29 @@ class Arr extends \Illuminate\Support\Arr
         }
 
         return $value;
+    }
+
+    public static function bracketGet(array $array, string $key, mixed $default = null): mixed
+    {
+        $keys = preg_split('/\[|\]\[|\]/', $key, -1, PREG_SPLIT_NO_EMPTY);
+        $current = $array;
+        foreach ($keys as $segment) {
+            if (!is_array($current) || !array_key_exists($segment, $current))
+                return $default;
+            $current = $current[$segment];
+        }
+        return $current;
+    }
+
+    public static function bracketSet(array &$array, string $key, mixed $value): void
+    {
+        $keys = preg_split('/\[|\]\[|\]/', $key, -1, PREG_SPLIT_NO_EMPTY);
+        $current = &$array;
+        foreach ($keys as $segment) {
+            if (!isset($current[$segment]) || !is_array($current[$segment]))
+                $current[$segment] = [];
+            $current = &$current[$segment];
+        }
+        $current = $value;
     }
 }
