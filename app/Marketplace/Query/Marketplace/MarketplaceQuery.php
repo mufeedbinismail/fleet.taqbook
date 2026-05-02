457685f939a262d819801df206349b6424139666
Mohamed Mufeed <mufeedbinismail@gmail.com>
2026-05-02 16:54:59 +0530

    refactor: move get_marketplace query to Marketplace/Query namespace

---
+++ app/Marketplace/Query/Marketplace/MarketplaceQuery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Marketplace\Query\Marketplace;
+
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\DB;
+
+class MarketplaceQuery
+{
+    public function builder(int|string $id): Builder
+    {
+        return DB::table('marketplaces as m')
+            ->join('chart_master as a', 'm.provision_account', '=', 'a.account_code')
+            ->leftJoin('suppliers as s', 'm.supplier_id', '=', 's.supplier_id')
+            ->where('m.id', $id)
+            ->select(
+                DB::raw('m.*'),
+                'a.account_name as provision_account_name',
+                's.supp_name as supplier_name',
+                's.tax_group_id',
+                's.tax_included',
+            );
+    }
+}
