20c43822a75dd7c882d0dc9dca2b70b5dc9da009
Mohamed Mufeed <mufeedbinismail@gmail.com>
2026-07-28 16:37:45 +0530

    feat: wire axios plugin to bundle

--- resources/js/plugins.js
+++ resources/js/plugins.js
@@ -1,5 +1,7 @@
 import jQuery from './plugins/jquery';
 import Alpine from './plugins/alpine';
+import axios  from './plugins/axios';
 
 window.$ = window.jQuery = jQuery;
 window.Alpine = Alpine;
+window.axios = axios
